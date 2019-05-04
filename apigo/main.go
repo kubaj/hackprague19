ffc32ec9192ce7f1ef7b9887de5b2d8dd078e9fc
Jakub Kulich <jakub.kulich@exponea.com>
2019-05-04 15:39:29 +0200

    feat(apigo): port from environment

--- apigo/main.go
+++ apigo/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"net/http"
-	
+	"os"
+
 	"github.com/labstack/echo"
 )
 
 func main() {
+	serverPort := os.Getenv("PORT")
+	if serverPort == "" {
+		serverPort = "8080"
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + serverPort))
 }
